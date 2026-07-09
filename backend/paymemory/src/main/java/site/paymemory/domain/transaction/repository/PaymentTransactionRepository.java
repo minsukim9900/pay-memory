55b73bc5033fcb55a1991d062a811b7909d3e8fb
minsukim9900 <mmsskksw9900@gmail.com>
2026-07-09 21:10:19 +0900

    refactor: 거래내역 Repository에 QueryDSL Custom Repository 연결
    - PaymentTransactionRepository에서 JPQL 기반 조회 메서드를 제거
    - PaymentTransactionCustomRepository를 상속하도록 수정
    - 거래내역 범위 조회를 QueryDSL Custom Repository 구현체로 위임

--- backend/paymemory/src/main/java/site/paymemory/domain/transaction/repository/PaymentTransactionRepository.java
+++ backend/paymemory/src/main/java/site/paymemory/domain/transaction/repository/PaymentTransactionRepository.java
@@ -1,28 +1,11 @@
 package site.paymemory.domain.transaction.repository;
 
-import java.time.Instant;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import site.paymemory.domain.transaction.entity.PaymentTransaction;
 
 public interface PaymentTransactionRepository extends
         JpaRepository<PaymentTransaction, Long>,
-        PaymentTransactionRepositoryPort {
-
-    @Override
-    @Query("""
-            SELECT paymentTransaction
-            FROM PaymentTransaction paymentTransaction
-            WHERE paymentTransaction.user.id = :userId
-              AND paymentTransaction.transactionAt BETWEEN :startTransactionAt AND :endTransactionAt
-            """)
-    List<PaymentTransaction> findByUserIdAndTransactionAtBetween(
-            @Param("userId") Long userId,
-            @Param("startTransactionAt") Instant startTransactionAt,
-            @Param("endTransactionAt") Instant endTransactionAt
-    );
+        PaymentTransactionRepositoryPort,
+        PaymentTransactionCustomRepository {
 }
